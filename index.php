f4c446a331696f69bab68e69c9dd7d9d48bd506a
ddvdv <vdv.david@gmail.com>
2017-05-23 12:58:31 +0200

    changement en index.html

---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+   <head>
+      <meta charset="utf-8">
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Kumar+One" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="normalize.css">
+      <link rel="stylesheet" type="text/css" href="perso.css">
+      <title>De l'importance des choses</title>
+   </head>
+   
+   <body>
+      <header>
+      <h1>Est-ce un <em class="bien">Bien</em>? Est-ce un <em class="mal">Mal</em>? Je ne sais pas.</h1>
+      <p>11 mai 2016, Alexandre Gorius</p>
+
+      <blockquote>Dans la nature, <em class="bien">Bien</em> & <em class="mal">Mal</em> n’existent pas. L'Univers lui-même n'est ni bon ni mauvais, mais un potentiel en développement permanent.</blockquote>
+      </header>
+
+      Dans la vie, il y a 4 types de personnes.
+      <ul>
+         <li>les gens qui pensent qu'Internet c'est <em class="bien">Bien</em>,</li>
+         <li>les gens qui pensent qu'Internet c'est le <em class="mal">Mal</em>,</li>
+         <li>les gens qui pensent qu'Internet ce sont des chats,</li>
+         <li>les gens qui n'y pensent pas.</li>
+      </ul>
+
+      <figure>
+         <img src='https://cdn-images-1.medium.com/max/1000/1*2PCmLZyzQaF2pyKYkSTFpA.jpeg' alt="photo d'un garnement vidant un extincteur"/>
+      </figure>
+
+      <p>Du point de vue de ce rocher sur Mars, rien ne se soucie de vos problèmes. Là haut, ça n’a pas d’importance, il n'y a aucun "problème", tout est comme cela doit être.</p>
+
+      <p>Ici bas, la seule réalité qui soit est celle de notre conscience. Rien n’existe si la conscience ne le pense pas. Rien n’existe non plus si nos sens ne transmettent pas le message. Cela signifie que si nous sentons <em class="mal">Mal</em> quelque chose, notre réalité change. C’est chose possible car notre perception est limitée: on ne peut pas voir à 360 degrés par exemple. Si nous pensons nos sensations d’une manière ou d’une autre, alors notre réalité change aussi en fonction de la manière. Ainsi, si nous pouvons autant changer notre réalité, alors rien n’est tangible.</p>
+
+      <p>Il y a des preuves à cela. Tombez amoureux de quelqu’un et vous aurez une vision quasi divine de cette personne. Sortez de son emprise, cette personne se révèle différente. Pourtant elle n’a pas changé, seulement vous avez changé d’état d’esprit. Tout fonctionne de cette manière : ce n’est pas seulement que vous pouvez activement façonner votre monde de cette manière, c’est que vous le faites passivement, quoi qu’il arrive.</p>
+
+      <figure>
+         <img src='http://static2.businessinsider.com/image/52fe8230eab8ea4275063b89/nasa-has-determined-where-the-mysterious-jelly-doughnut-rock-on-mars-came-from.jpg' alt="photo d'un rocher sur Mars qui n'en a rien à caler"/>
+      </figure>
+
+      <p>Le rocher sur Mars ne fait pas la différence entre <em class="bien">Bien</em> et <em class="mal">Mal</em>, car pour lui, ni l’un ni l’autre n’existent.</p>
+
+      <p>Tout est relatif à un point de vue. Ainsi, il n’existe pas de <em class="bien">Bien</em> ni de <em class="mal">Mal</em>, car <em class="bien">Bien</em> et <em class="mal">Mal</em> sont des valeurs absolues. Si quelque chose est bon d’après quelqu’un et <em class="mal">Mal</em> d’après quelqu’un d’autre, alors cette chose n’est en elle-même ni bonne, ni <em class="mal">Mal</em>. Rappelez-vous : d’après ce rocher sur Mars, il n’y a pas de séparation entre <em class="bien">Bien</em> et <em class="mal">Mal</em>, les deux n’existent jute pas.</p>
+
+      <h2>“Est-ce un <em class="bien">Bien</em>? Est-ce un <em class="mal">Mal</em>? Je ne sais pas.”</h2>
+
+      <p>Une histoire chinoise célèbre illustre parfaitement ce propos.</p>
+
+      <figure>
+         <img src="https://cdn-images-1.medium.com/max/800/1*IQqkmPXYZuJViY5p-ymk0A.jpeg" alt="Le vieux fermier chinois"/>
+      </figure>
+
+      <p>C’est l’histoire d’un fermier qui possède un cheval. Un jour, le cheval s’enfuit. Les villageois s’ameutent à la maison du fermier pour lui remonter le moral. <q class="villageois">Quelle <em class="mal">Mal</em>chance ! Ton seul cheval ! Nous sommes vraiment désolé pour cette perte, c’est si triste !</q></p>
+
+      <p>Le fermier répond, <q class="fermier">Est-ce un <em class="bien">Bien</em>? Est-ce un <em class="mal">Mal</em>? Je ne sais pas.</q></p>
+
+      <p>Le jour suivant, le cheval revient avec sept magnifiques juments. Les villageois reviennent pour célébrer la bonne nouvelle. <q class="villageois">Quelle chance incroyable ! Avec huit chevaux, tu es un homme riche ! Tu dois être ravis de cette nouvelle !</q></p>
+
+      <p>Le fermier répond, <q class="fermier">Est-ce un <em class="bien">Bien</em>? Est-ce un <em class="mal">Mal</em>? Je ne sais pas.</q></p>
+
+      <p>Le jour d’après, le fils du fermier s’occupe de l’entraînement des nouveaux chevaux. Mais l’un d’entre eux s’est révélé agressif; le garçon tombe et se casse la jambe. Les villageois reviennent. <q class="villageois">Pauvre enfant ! Nous sommes si désolé pour ce qui est arrivé ! Nous espérons qu’il s’en remettra <em class="bien">Bien</em>tôt, c’est tellement dommage…</q></p>
+
+      <p>Le fermier répond, <q class="fermier">Est-ce un <em class="bien">Bien</em>? Est-ce un <em class="mal">Mal</em>? Je ne sais pas.</q></p>
+
+      <p>Le quatrième jour, l’armée arrive au village à la recherche de nouvelles jeunes recrues. Grâce à sa jambe cassée, le fils du fermier échappe au recrutement. <q class="villageois">Quelle nouvelle incroyable ! Ton fils échappera à cette guerre sanglante, il vivra, à tes côtés ! Tu dois être tellement heureux, c’est fantastique !</q></p>
+
+      <p>Quand <em class="bien">Bien</em> même, le fermier répond, <q class="fermier">Est-ce un <em class="bien">Bien</em>? Est-ce un <em class="mal">Mal</em>? Je ne sais pas.</q></p>
+
+      ----
+
+      <p>En résumé, représentons ce dialogue sous forme de tableau</p>
+
+      <table>
+                  <caption> Tableau représentant la philosophie du fermier.</caption>
+         <tr>
+            <th>Evénement</th>
+            <th><em class="bien">Bien</em> ou <em class="mal">Mal</em>?</th>
+         </tr>
+         <tr>
+            <td>Perdre son cheval 
+            </td>
+            <td>Cela dépend
+            </td>
+         </tr>
+         <tr>
+            <td>Gagner 7 chevaux 
+            </td>
+            <td>Cela dépend
+            </td>
+         </tr>
+         <tr>
+            <td>Le fils se blesse
+            </td>
+            <td>Cela dépend
+            </td>
+         </tr>
+         <tr>
+            <td>Evite la guerre 
+            </td>
+            <td>Cela dépend
+            </td>
+         </tr>
+      </table>
+
+      <p>
+      Vous trouverez ici <a target="_blank" title="La lib'" href="http://www.lalibre.be">un contre-exemple</a> accordant une illusion d'importance à un peu tout et n'importe quoi...<br>
+      Un exemple encore plus extrême pour être trouvé sur <a target="_blank" title="le market" href="https://www.amazon.com/">le lien suivant</a>
+      </p>
+       
+      <h2>Conclusion</h2>
+
+      <p>Le <em class="bien">Bien</em> et le <em class="mal">Mal</em> sont des valeurs culturelles et non naturelles. La banane est bonne pour le singe, le singe n'est pas bon pour la banane. N'en faisons donc pas un fromage...</p>
+
+
+       <footer>  
+      <hr>----
+      Copyright Just Another Company 2017. Tous droits réservés. Très largement adapté de cet article: https://medium.com/scribe/il-ny-a-rien-de-<em class="bien">Bien</em>-ni-de-<em class="mal">Mal</em>-3d46a2d2e263
+      </footer>
+
+   </body>
+</html>
